e196376e330532e1ad11dbdbf58b9406c1669ad2
Daniël le Roux <danielleroux25@gmail.com>
2020-05-08 11:57:02 +0200

    5_RR_ Added enemy. Printed Coordinates to console

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Block waypoint in path)
+        {
+            print(waypoint.name);
+        }
+    }
+}
