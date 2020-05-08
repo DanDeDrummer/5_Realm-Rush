4adf52a382868ff92fc612447ec26f29a7d23c3f
Daniël le Roux <danielleroux25@gmail.com>
2020-05-08 17:11:57 +0200

    5_RR_Made Waypoints independent of Cube Editor

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
-    TextMesh textMesh;
+    Waypoint waypoint;
 
-
-
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
     // Update is called once per frame
     void Update()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+        SnapToGrid();
+        UpdateLabel();
+    }
 
-        textMesh = GetComponentInChildren<TextMesh>();
-        string lableText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(waypoint.GetGridPos().x, 0f, waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        Vector3 gridPos = waypoint.GetGridPos();
+        string lableText = gridPos.x/ gridSize + "," + gridPos.y/ gridSize;
         gameObject.name = lableText;
         textMesh.text = lableText;
     }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2Int gridPos;
+    const int gridSize = 10;
+
+
+    public int GetGridSize()
     {
-        
+        return gridSize;
+    }
+
+    public Vector2 GetGridPos()
+    {
+        int x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        int y = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        return new Vector2Int(x, y);
     }
 
     // Update is called once per frame
