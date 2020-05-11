024b8a6d4ed2e3101e3751942f22d35773324341
Daniël le Roux <danielleroux25@gmail.com>
2020-05-11 13:24:22 +0200

    5_RR_Deleted Bullet Prefab to avoid future errors. Reprefabbed tower with gun attached

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] GameObject damageEffect;
+    [SerializeField] int hitpoints = 50;
+    int damageDealt;
+    Tower tower;
+
+    private void Awake()
+    {
+        tower = FindObjectOfType<Tower>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start hitpoints: " + hitpoints);
+    }
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Ow on collision by " + other.name);
+        damageDealt = tower.GetDamageDealt();
+        print(damageDealt);
+        TakeDamage(damageDealt);       
+    }
+
+    private void TakeDamage(int damageDealt)
+    {
+        //Damege effect triggered
+        damageEffect.SetActive(true);
+
+        //Lose Hitpoints
+        hitpoints = hitpoints - damageDealt;
+        
+
+        //die if hitpoints below 0
+        if (hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    [SerializeField] int damageDealt = 1;
+    bool isActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt();
+        isActive = !isActive;
+        //ShootEnemy(isActive);
+    }
+
+    private void LookAt()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+
+    /*private void ShootEnemy(bool isActive)
+    {
+        var particleSystem = turret.GetComponent<ParticleSystem>().emission;
+        particleSystem.enabled = isActive;       
+    }*/
+
+    public int GetDamageDealt()
+    {
+        return damageDealt;
+    }
+
+ 
+}
