3cdd368f2c008055b8ec61a27ac46ea171d950e4
Daniël le Roux <danielleroux25@gmail.com>
2020-05-11 15:48:23 +0200

    5_RR_Beginnings of EnemySpawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 5f;
+    [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] int numberEnemiesInLevel = 0, maxNumberEnemiesInLevel = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (numberEnemiesInLevel < maxNumberEnemiesInLevel)
+        {
+            StartCoroutine(RepeatedlySpawnEnemies());
+        }
+
+        else
+        {
+            StopCoroutine(RepeatedlySpawnEnemies());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true)//forever
+        {
+            print("Spawning");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            //Instantiate(enemyPrefab, gameObject.transform.position, Quaternion.identity);
+           // numberEnemiesInLevel++;
+            //print("number hostiles in level: " + numberEnemiesInLevel);
+        }
+
+    }
+}
